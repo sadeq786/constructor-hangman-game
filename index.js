29e5782d98cb151582005aa8d959bf48e52e6630
Sadeq Rahman <sadeq@dhcp-10-102-210-158.wireless.northwestern.private>
2018-05-16 17:28:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Don't forget to start with npm init --y to get all the 
+// standard node stuff like package.json. 
+
+// You will need an inquirer here to display the initial question. 
+inquirer (prompt([
+    
+]))
